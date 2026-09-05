291e8e4066ba2eed76ada66b0d1fa7db230c3460
hamednikravesh3-sys <hamednikravesh3@gmail.com>
2026-09-05 19:03:04 +0330

    Wire weather route signals into NV platform

--- app/src/main/java/ir/nv/navigation/navigation/NvNavigationPlatform.kt
+++ app/src/main/java/ir/nv/navigation/navigation/NvNavigationPlatform.kt
@@ -4,6 +4,7 @@ import ir.nv.navigation.ai.route.NvAdaptiveRouteRanker
 import ir.nv.navigation.online.OnlineNavigationService
 import ir.nv.navigation.routing.AStarRouter
 import ir.nv.navigation.traffic.LiveTrafficService
+import ir.nv.navigation.weather.WeatherRouteSignalProvider
 
 class NvNavigationPlatform(
     onlineService: OnlineNavigationService,
@@ -14,7 +15,8 @@ class NvNavigationPlatform(
         onlineProvider = OnlineRouteProviderAdapter(onlineService),
         offlineProvider = OfflineRouteProviderAdapter(routerProvider),
         trafficProvider = LiveTrafficProviderAdapter(liveTrafficService),
-        ranker = NvAdaptiveRouteRanker()
+        ranker = NvAdaptiveRouteRanker(),
+        signalProvider = WeatherRouteSignalProvider()
     )
 
     val reroutePolicy = ContinuousReroutePolicy()
