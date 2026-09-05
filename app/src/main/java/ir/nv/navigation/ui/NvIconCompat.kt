858be87d70fdc091494c2b27865098cad3944842
hamednikravesh3-sys <hamednikravesh3@gmail.com>
2026-09-05 08:06:49 +0330

    Add compatible U-turn icon alias

---
+++ app/src/main/java/ir/nv/navigation/ui/NvIconCompat.kt
@@ -0,0 +1,8 @@
+package ir.nv.navigation.ui
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.TurnLeft
+import androidx.compose.ui.graphics.vector.ImageVector
+
+val Icons.Rounded.UturnLeft: ImageVector
+    get() = Icons.Rounded.TurnLeft
