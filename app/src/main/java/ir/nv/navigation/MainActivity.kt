2549f77c02d79704997988e32d14a7b2aaa05b2e
hamednikravesh3-sys <hamednikravesh3@gmail.com>
2026-09-06 06:02:51 +0330

    Switch NV to V13 bookmark gated QR flow

--- app/src/main/java/ir/nv/navigation/MainActivity.kt
+++ app/src/main/java/ir/nv/navigation/MainActivity.kt
@@ -13,7 +13,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModelProvider
 import ir.nv.navigation.navigation.service.NvNavigationService
-import ir.nv.navigation.ui.NvReferenceV12
+import ir.nv.navigation.ui.NvReferenceV13
 import ir.nv.navigation.ui.NvViewModel
 import ir.nv.navigation.ui.theme.AppThemeMode
 import ir.nv.navigation.ui.theme.NvTheme
@@ -62,7 +62,7 @@ class MainActivity : ComponentActivity() {
             }
 
             NvTheme(darkTheme = darkMode) {
-                NvReferenceV12(
+                NvReferenceV13(
                     darkMode = darkMode,
                     themeMode = themeMode,
                     onThemeModeChange = { selected ->
