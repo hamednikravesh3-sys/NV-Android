44be31a919a8e2a4bf6ace1009edec5c90da23af
hamednikravesh3-sys <hamednikravesh3@gmail.com>
2026-09-06 04:41:28 +0330

    Add NV code registry runtime config

---
+++ app/src/main/java/ir/nv/navigation/data/NvCodeConfig.kt
@@ -0,0 +1,6 @@
+package ir.nv.navigation.data
+
+object NvCodeConfig {
+    /** Set this to the deployed NV Code Registry base URL, e.g. https://nv-code-registry.example.workers.dev */
+    const val REGISTRY_BASE_URL: String = ""
+}
