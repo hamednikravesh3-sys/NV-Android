edf27c65681598f1a8cf5819e9eff45fba6a79e0
hamednikravesh3-sys <hamednikravesh3@gmail.com>
2026-09-08 03:53:17 +0330

    Test NV cloud sync payload validation

---
+++ app/src/test/java/ir/nv/navigation/cloud/NvCloudSyncClientTest.kt
@@ -0,0 +1,37 @@
+package ir.nv.navigation.cloud
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFails
+import org.junit.Test
+
+class NvCloudSyncClientTest {
+    private val client = NvCloudSyncClient()
+
+    @Test
+    fun parsesAndDeduplicatesCloudPlaces() {
+        val snapshot = client.parseSnapshot(
+            """{"revision":7,"places":[{"code":"1845623","name":"خانه","latitude":35.7,"longitude":51.4,"updatedAt":100},{"code":"1845623","name":"خانه تکراری","latitude":35.7,"longitude":51.4,"updatedAt":101}]}"""
+        )
+        assertEquals(7L, snapshot.revision)
+        assertEquals(1, snapshot.places.size)
+        assertEquals("1845623", snapshot.places.single().code)
+    }
+
+    @Test
+    fun rejectsInvalidCoordinate() {
+        assertFails {
+            client.parseSnapshot(
+                """{"revision":1,"places":[{"code":"12","name":"x","latitude":120.0,"longitude":51.4,"updatedAt":1}]}"""
+            )
+        }
+    }
+
+    @Test
+    fun rejectsNonNumericNvCode() {
+        assertFails {
+            client.parseSnapshot(
+                """{"revision":1,"places":[{"code":"NV-12","name":"x","latitude":35.7,"longitude":51.4,"updatedAt":1}]}"""
+            )
+        }
+    }
+}
